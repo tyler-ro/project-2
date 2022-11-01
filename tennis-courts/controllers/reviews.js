ea9352205ec891fcd4b970e8e99e669cd9720604
Tyler Ro <tyler@Laptop.local>
2022-10-31 22:18:59 -0400

    bugfix on create function

--- tennis-courts/controllers/reviews.js
+++ tennis-courts/controllers/reviews.js
@@ -1,16 +1,13 @@
 const Location = require("../models/location");
 
-//   console.log(req.params.id);
 function create(req, res) {
-  //     Location.findById(req.params.id, function (err, locations) {
-  //       if (err) return res.send(err.message);
-  //       locations.reviews.push(req.body);
-  //       locations.save(function (err) {
-  //         if (err) return res.send(err.message);
-  // res.redirect(`/location/${locations._id}`);
-  //       });
-  //     });
-  //   }
-  res.redirect("/");
+  Location.findById(req.params.id, function (err, locations) {
+    if (err) return res.send(err.message);
+    Location.find({ _id: req.params.id }, function (err) {
+      if (err) return res.send(err.message);
+      res.redirect("/");
+    });
+  });
 }
+
 module.exports = { create };
